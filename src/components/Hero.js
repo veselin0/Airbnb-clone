f7de513b916f36045a0934485fd1446739d0ee09
Veselin <vesselin60@gmail.com>
2021-12-16 13:01:44 +0200

    Add main section: Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+const Hero = () => {
+    return (
+        <section className="hero">
+            <img
+                className="group77"
+                src="../images/photo-grid.png"
+                alt="some views of Nature"
+            />
+            <h1 className="title">Online Experiences</h1>
+            <p className="text">
+                Join unique interactive activities led by one-of-a-kind
+                hosts—all without leaving home.
+            </p>
+        </section>
+    );
+};
+
+export default Hero;
